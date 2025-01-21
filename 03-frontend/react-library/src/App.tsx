c791f33f192d1caf3f14b56750f53b5eabc20b75
Eren Sönmez <testuser@gmail.com>
2025-01-21 17:37:54 +0300

    react

---
+++ 03-frontend/react-library/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'; import './App.css';
+import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+import { Footer } from './layouts/NavbarAndFooter/Footer';
+import { Homepage } from './layouts/Homepage/Homepage';
+
+export const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Homepage/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
