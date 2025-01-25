faebb0267788c15b573c017e7e7550f98875cbe3
Eren Sönmez <testuser@gmail.com>
2025-01-25 15:14:12 +0300

    Search methods are added

--- 02-backend/spring-boot-library/src/main/java/com/luvienze/spring_boot_library/dao/BookRepository.java
+++ 02-backend/spring-boot-library/src/main/java/com/luvienze/spring_boot_library/dao/BookRepository.java
@@ -4,7 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.luvienze.spring_boot_library.entity.Book;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 @CrossOrigin(origins = "http://localhost:3000", methods = RequestMethod.GET)
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
+
+    Page<Book> findByCategory(@RequestParam("category") String category, Pageable pageable);
 }
