a5abe53c9fe439d7b3951b5f2c645884acbd44fc
Eren Sönmez <testuser@gmail.com>
2025-01-25 18:45:01 +0300

    review repository is created

---
+++ 02-backend/spring-boot-library/src/main/java/com/luvienze/spring_boot_library/dao/ReviewRepository.java
@@ -0,0 +1,15 @@
+package com.luvienze.spring_boot_library.dao;
+
+import com.luvienze.spring_boot_library.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@CrossOrigin(origins = "http://localhost:3000", methods = RequestMethod.GET)
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    Page<Review> findBookById(@RequestParam("book_id") Long bookId, Pageable pageable);
+}
