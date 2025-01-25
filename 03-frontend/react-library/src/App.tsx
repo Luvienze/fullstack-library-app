9524ecbcbcbeb8bb9f050b9f2ae041b32b23242d
Eren Sönmez <testuser@gmail.com>
2025-01-25 15:59:26 +0300

    react router is installed and implemented

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -1,17 +1,32 @@
-import React from 'react'; 
+import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { Homepage } from './layouts/Homepage/Homepage';
 import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 export const App = () => {
   return (
-    <div>
+    <div className='d-flex flex-column min-vh-100'>
       <Navbar />
-      {/* <Homepage/> */}
-      <SearchBooksPage/>
-      <Footer/>
+      <div className='flex-grow-1'>
+        <Switch>
+          <Route path='/' exact>
+            <Redirect to='/home' />
+          </Route>
+
+          <Route path='/home'>
+            <Homepage />
+          </Route>
+
+          <Route path='/search'>
+            <SearchBooksPage />
+          </Route>
+        </Switch>
+
+      </div>
+      <Footer />
     </div>
   );
 }
